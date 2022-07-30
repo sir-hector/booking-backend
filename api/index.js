65f5411c807f6c9317efc1c3348bc7ab29b142eb
sir-hector <krauskarol7@gmail.com>
2022-07-30 14:53:38 +0200

    add API endpoints for hotels

--- api/index.js
+++ api/index.js
@@ -23,6 +23,8 @@ mongoose.connection.on("disconnected", () => {
 })
 
 //middlewares
+
+app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
