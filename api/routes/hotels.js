0247016ef51760e23a710a58cc46dec7cdebda69
sir-hector <krauskarol7@gmail.com>
2022-07-30 13:07:43 +0200

    add Hotel model

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,7 +2,8 @@ import express from 'express'
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+// CREATE
+router.post("/", (req, res) => {
     res.send("Hello, this is hotels endpoit")
 })
 
