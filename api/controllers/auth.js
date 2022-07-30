4dcb39488f7a9ffaaa8f610069394db456bf12a7
sir-hector <krauskarol7@gmail.com>
2022-07-30 21:12:09 +0200

    add login controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt'
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
     try {
@@ -13,6 +14,20 @@ export const register = async (req, res, next) => {
         })
         await newUser.save()
         res.status(200).send("user has been created")
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        if(!user) return next(createError(404, "User not found"))
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if(!isPasswordCorrect) return next(createError(400, "Wrong password or username"))
+        const {password, isAdmin, ...otherDetails} = user._doc
+        res.status(200).json({...otherDetails})
         
     } catch (error) {
         next(error)
