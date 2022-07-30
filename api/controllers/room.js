ffb1e941c099fddb97c1554a7e890ed9f3e8997f
sir-hector <krauskarol7@gmail.com>
2022-07-30 23:25:51 +0200

    add create room endpoint

---
+++ api/controllers/room.js
@@ -0,0 +1,21 @@
+import User from "../models/Room.js"
+import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+import Room from "../models/Room.js";
+
+export const createRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    const newRoom = new Room(req.body);
+
+    try {
+        const savedRoom = await newRoom.save()
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {$push : {rooms: savedRoom._id}})
+        } catch (error) {
+            next(error)
+        }
+        res.status(200).json(savedRoom)
+    } catch (error) {
+        next(error)
+    }
+}
