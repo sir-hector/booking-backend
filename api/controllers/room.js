3963e36055fd7db34a0dbbe87e7d27fac883d5be
sir-hector <krauskarol7@gmail.com>
2022-07-31 14:19:01 +0200

    add endpoints for rooms

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -1,4 +1,3 @@
-import User from "../models/Room.js"
 import Hotel from "../models/Hotel.js"
 import { createError } from "../utils/error.js";
 import Room from "../models/Room.js";
@@ -17,5 +16,44 @@ export const createRoom = async (req, res, next) => {
         res.status(200).json(savedRoom)
     } catch (error) {
         next(error)
+    }
+}
+
+export const updateRoom = async (req, res, next) => {
+    try {
+        const updatedRoom = await Room.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true});
+        res.status(200).json(updatedRoom)
+    }catch(err){
+        next(err)
+    }
+}
+export const deleteRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    try {
+        await Room.findByIdAndDelete(req.params.id);
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {$pull : {rooms: req.params.id}})
+        } catch (error) {
+            next(error)
+        }
+        res.status(200).json("Hotel has been deleted")
+    }catch(err){
+        next(err)
+    }
+}
+export const getRoom = async (req, res, next) => {
+    try {
+        const room = await Room.findById(req.params.id);
+        res.status(200).json(room)
+    }catch(err){
+        next(err)
+    }
+}
+export const getAllRoom = async (req, res, next) => {
+    try {
+        const rooms = await Room.find();
+        res.status(200).json(rooms)
+    }catch(err){
+        next(err)
     }
 }
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import { createRoom, updateRoom, deleteRoom, getRoom, getAllRoom } from '../controllers/room.js';
+import { verifyToken, verifyUser, isAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello, this is rooms endpoit")
-})
+// CREATE
+router.post("/:hotelid", isAdmin, createRoom)
+
+// UPDATE
+router.put("/:id",isAdmin, updateRoom)
+
+// DELETE
+router.delete("/:id/:hotelid",isAdmin, deleteRoom)
+
+// GET
+router.get("/:id", getRoom)
+
+// GET ALL
+router.get("/", getAllRoom)
 
 export default router;
